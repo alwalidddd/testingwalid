271f75a89eb3c741434708a109417d82f38c6045
alwalidddd <93113054+alwalidddd@users.noreply.github.com>
2023-04-27 10:21:21 +0700

    Add files via upload

---
+++ deploy.groovy
@@ -0,0 +1,43 @@
+pipeline {
+  agent any
+  
+  stages {
+    stage('Clone Repository') {
+      steps {
+        //git 'https://github.com/alwalidddd/your-repo.git'
+        git 'https://github.com/alwalidddd/testingwalid'
+      }
+    }
+    
+    stage('Build Docker Image') {
+      steps {
+        script {
+          docker.withRegistry('https://your-docker-registry', 'docker-credentials-id') {
+            def customImage = docker.build('your-image-name')
+            customImage.push()
+          }
+        }
+      }
+    }
+    
+    stage('Deploy to Docker') {
+      steps {
+        script {
+          docker.withRegistry('https://your-docker-registry', 'docker-credentials-id') {
+            def container = docker.image('nginx').run('-p 80:80 -d')
+            // Additional deployment steps (e.g., environment setup) can be added here
+          }
+        }
+      }
+    }
+  }
+  
+  post {
+    success {
+      echo 'Deployment successful!'
+    }
+    failure {
+      echo 'Deployment failed!'
+    }
+  }
+}
